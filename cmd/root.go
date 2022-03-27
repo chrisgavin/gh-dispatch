9a08373e2bbb078c379fa857d506a85e654d41d1
Chris Gavin <chris@chrisgavin.me>
2022-03-27 20:11:31 +0100

    Watch workflows after starting them.

--- cmd/root.go
+++ cmd/root.go
@@ -3,10 +3,14 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/chrisgavin/gh-dispatch/internal/dispatcher"
 	"github.com/chrisgavin/gh-dispatch/internal/locator"
+	"github.com/chrisgavin/gh-dispatch/internal/run"
 	"github.com/chrisgavin/gh-dispatch/internal/version"
 	"github.com/cli/go-gh"
 	"github.com/go-git/go-git/v5"
@@ -16,6 +20,7 @@ import (
 )
 
 type rootFlagFields struct {
+	noWatch bool
 }
 
 var rootFlags = rootFlagFields{}
@@ -82,10 +87,37 @@ var rootCmd = &cobra.Command{
 		}
 		reference := head.Name().String()
 
+		log.Info("Dispatching workflow...")
 		err = dispatcher.DispatchWorkflow(currentRepository, reference, workflowName, inputAnswers)
 		if err != nil {
 			return err
 		}
+
+		if !rootFlags.noWatch {
+			log.Info("Waiting for workflow to start...")
+			workflowRun, err := run.LocateRun(currentRepository, reference)
+			if err != nil {
+				return err
+			}
+
+			command := exec.CommandContext(cmd.Context(), "gh", "run", "watch", strconv.FormatInt(workflowRun.ID, 10))
+			command.Stdout = os.Stdout
+			command.Stderr = os.Stderr
+			err = command.Run()
+			if err != nil {
+				return errors.Wrap(err, "Unable to watch workflow progress.")
+			}
+
+			workflowRun, err = run.GetRun(currentRepository, workflowRun.ID)
+			if err != nil {
+				return err
+			}
+			log.Infof("Workflow completed with conclusion %s.", workflowRun.Conclusion)
+			if workflowRun.Conclusion != "success" {
+				os.Exit(1)
+			}
+		}
+
 		return nil
 	},
 }
@@ -102,6 +134,8 @@ func (f *rootFlagFields) Init(cmd *cobra.Command) error {
 }
 
 func Execute(ctx context.Context) error {
+	rootCmd.Flags().BoolVar(&rootFlags.noWatch, "no-watch", false, "Do not wait for the workflow to complete.")
+
 	err := rootFlags.Init(rootCmd)
 	if err != nil {
 		return err
