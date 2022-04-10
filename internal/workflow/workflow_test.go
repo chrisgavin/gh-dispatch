7e6da2e08e756c876cdb80331dac4bbbeef0672f
Chris Gavin <chris@chrisgavin.me>
2022-04-10 16:10:36 +0100

    Handle different workflow input types.

--- internal/workflow/workflow_test.go
+++ internal/workflow/workflow_test.go
@@ -8,27 +8,27 @@ import (
 
 func parseTestWorkflow(t *testing.T, workflowContent string) *Workflow {
 	const workflowName = "test.yml"
-	workflow, err := ReadWorkflow(workflowName, []byte(workflowContent))
+	workflowData, err := ReadWorkflow(workflowName, []byte(workflowContent))
 	require.NoError(t, err)
-	require.Equal(t, workflowName, workflow.Name)
-	return workflow
+	require.Equal(t, workflowName, workflowData.Name)
+	return workflowData
 }
 
 func TestReadNonDispatchableWorkflow(t *testing.T) {
 	const workflowContent = `
 on: push	
 `
-	workflow := parseTestWorkflow(t, workflowContent)
-	require.False(t, workflow.Dispatchable)
+	workflowData := parseTestWorkflow(t, workflowContent)
+	require.False(t, workflowData.Dispatchable)
 }
 
 func TestReadDispatchableWorkflowSingletonStyle(t *testing.T) {
 	const workflowContent = `
 on: workflow_dispatch	
 `
-	workflow := parseTestWorkflow(t, workflowContent)
-	require.True(t, workflow.Dispatchable)
-	require.Empty(t, workflow.Inputs)
+	workflowData := parseTestWorkflow(t, workflowContent)
+	require.True(t, workflowData.Dispatchable)
+	require.Empty(t, workflowData.Inputs)
 }
 
 func TestReadDispatchableWorkflowListStyle(t *testing.T) {
@@ -38,9 +38,9 @@ on:
   - pull_request
   - workflow_dispatch	
 `
-	workflow := parseTestWorkflow(t, workflowContent)
-	require.True(t, workflow.Dispatchable)
-	require.Empty(t, workflow.Inputs)
+	workflowData := parseTestWorkflow(t, workflowContent)
+	require.True(t, workflowData.Dispatchable)
+	require.Empty(t, workflowData.Inputs)
 }
 
 func TestReadDispatchableWorkflowMapStyle(t *testing.T) {
@@ -50,9 +50,9 @@ on:
   pull_request: {}
   workflow_dispatch: {}
 `
-	workflow := parseTestWorkflow(t, workflowContent)
-	require.True(t, workflow.Dispatchable)
-	require.Empty(t, workflow.Inputs)
+	workflowData := parseTestWorkflow(t, workflowContent)
+	require.True(t, workflowData.Dispatchable)
+	require.Empty(t, workflowData.Inputs)
 }
 
 func TestReadWorkflowWithInputs(t *testing.T) {
@@ -64,16 +64,63 @@ on:
       some_input_with_description:
         description: "Some input description."
 `
-	workflow := parseTestWorkflow(t, workflowContent)
-	require.True(t, workflow.Dispatchable)
-	require.Equal(t, workflow.Inputs, []Input{
+	workflowData := parseTestWorkflow(t, workflowContent)
+	require.True(t, workflowData.Dispatchable)
+	require.Equal(t, []Input{
 		{
 			Name:        "some_input",
 			Description: "",
+			Type:        StringInput,
 		},
 		{
 			Name:        "some_input_with_description",
 			Description: "Some input description.",
+			Type:        StringInput,
 		},
-	})
+	}, workflowData.Inputs)
+}
+
+func TestReadWorkflowWithBooleanInputs(t *testing.T) {
+	const workflowContent = `
+on:
+  workflow_dispatch:
+    inputs:
+      some_input:
+        type: boolean
+`
+	workflowData := parseTestWorkflow(t, workflowContent)
+	require.True(t, workflowData.Dispatchable)
+	require.Equal(t, 1, len(workflowData.Inputs))
+	require.Equal(t, BooleanInput, workflowData.Inputs[0].Type)
+}
+
+func TestReadWorkflowWithChoiceInputs(t *testing.T) {
+	const workflowContent = `
+on:
+  workflow_dispatch:
+    inputs:
+      some_input:
+        type: choice
+        options: [foo, bar]
+`
+	workflowData := parseTestWorkflow(t, workflowContent)
+	require.True(t, workflowData.Dispatchable)
+	require.Equal(t, 1, len(workflowData.Inputs))
+	require.Equal(t, ChoiceInput, workflowData.Inputs[0].Type)
+	require.Equal(t, []string{"foo", "bar"}, workflowData.Inputs[0].OptionProvider())
+}
+
+func TestReadWorkflowWithEnvironmentInputs(t *testing.T) {
+	const workflowContent = `
+on:
+  workflow_dispatch:
+    inputs:
+      some_input:
+        type: environment
+`
+	workflowData, err := ReadWorkflow("test.yml", []byte(workflowContent))
+	require.NoError(t, err)
+	require.True(t, workflowData.Dispatchable)
+	require.Equal(t, 1, len(workflowData.Inputs))
+	require.Equal(t, EnvironmentInput, workflowData.Inputs[0].Type)
 }
